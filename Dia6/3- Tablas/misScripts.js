af2df1e4dd0bc2e8c6766c5678925d535252228a
Luis <luism.montanezc@gmail.com>
2025-03-05 23:10:15 -0500

    bucles, for, while, do while, for of

--- Dia6/3- Tablas/misScripts.js
+++ Dia6/3- Tablas/misScripts.js
@@ -1,3 +1,23 @@
 function multiplicar(){
-  dddd
+  //Obtener el número
+  let elementoTextoTabla = document.getElementById("textoTabla");
+  let textoNumeroTabla = elementoTextoTabla.value;
+  let numeroTabla = Number(textoNumeroTabla);
+  
+  //Obtener la tabla
+  let elementoTablaMultiplicar = document.getElementById("listaTabla");
+
+  //Producir y mostrar resultados
+  for(x=1; x<=10; x++){
+    //calcular el resultado
+    let numeroResultado = numeroTabla * x;
+
+    //Armar un String con el resulado
+    let textoResultado = numeroTabla + " X " + x +" = " + numeroResultado;
+
+    //Crear un elemento a la lista
+    let itemLista = document.createElement("li");
+    itemLista.innerText = textoResultado;
+    elementoTablaMultiplicar.appendChild(itemLista);
+  }
 }
